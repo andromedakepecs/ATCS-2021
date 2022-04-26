f52af7dc168ae21ac4f5c098769be359aa648c54
Andromeda Kepecs <amkepecs@gmail.com>
2022-04-26 13:40:07 -0700

    Working basic mechanics with player only

--- connect-four/connect_four.py
+++ connect-four/connect_four.py
@@ -27,14 +27,37 @@ class Game():
 				print(SPACING.join(str(self.board[col][row])), end = ' ')
 			print()
 
-	def place_player(self, player, row, column):
-		pass
+	def place_player(self, player, column):
+		row = self.get_next_row(column)
+		(self.board[column])[row] = player
 
-	def is_valid_move(self, row, column):
-		pass
+	def is_valid_move(self, column):
+		if column >= COLUMNS or column < 0:
+			return False
+		elif (self.board[column])[0] != EMPTY:
+			return False
+		else:
+			row = self.get_next_row(column)
+			if (self.board[column])[row] == EMPTY:
+				return True
+		return False
+
+	def get_next_row(self, column):
+		row = -1
+		while (self.board[column])[row] != EMPTY: # Traverse rows in reverse order
+			row -= 1
+		return row
 
 	def player_turn(self, player):
-		pass
+		print(player + '\'s Turn')
+		valid = self.is_valid_move(COLUMNS + 1)
+		while not valid:
+			column = int(input('Enter a column: '))
+			valid = self.is_valid_move(column)
+			if not valid:
+				print('Please enter a valid move.')
+		return column
+
 
 	def ai_turn(self, player):
 		pass
@@ -43,11 +66,13 @@ class Game():
 		pass
 
 	def play_game(self):
-		pass
+		self.place_player(RED, self.player_turn(RED))
+		self.print_board()
 
 def main():
 	game = Game()
 	game.play_game()
+
 
 if __name__ == '__main__':
 	main()
